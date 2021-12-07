411ba42e8923d15dcea673a560a422624696c574
Surabordin <bluelimec@gmail.com>
2021-12-07 17:05:49 +0700

    lab9_3 complete

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age,high,property;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age <= 20){
+        cout << "Enter your height: ";
+        cin >> high;
+        if(high < 160){
+            cout << "Your status = UNFRIEND";
+        }else if(high < 175){
+            cout << "Your status = FRIEND";
+        }else{
+            cout << "Enter your property: ";
+            cin >> property;
+            if(property > 69e6){
+                cout << "Your status = MARRIED";
+            }else{ 
+                cout << "Your status = ONE-NIGHT-STAND";
+            }
+        }
+    }else{
+        if(age<30){
+            cout << "Enter your property: ";
+            cin >> property;
+            if(property>10e6){
+                cout << "Your status = BEST FRIEND";
+            }else{
+                cout << "Your status = UNFRIEND";
+            }
+        }else{
+            cout << "Your status = UNFRIEND";
+        }
+    }   
+}
+
+
+
+
+
 /*
 "Enter your age: "
 "Enter your height: "
